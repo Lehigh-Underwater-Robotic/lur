05f8877afede566197aedf3f3a1a1498bac6e74a
Nathaniel Todd-Long <ntoddlong@gmail.com>
2022-10-06 16:13:05 +0000

    Added a few build and docker scripts

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# rm -rf build
+# rm -rf install
+
+#rosdep install -i --from-path brain/src --rosdistro foxy -y
+#rosdep install -i --from-path sub/src --rosdistro foxy -y
+#rosdep install -i --from-path test/src --rosdistro foxy -y
+
+rosdep install -i --from-path src --rosdistro foxy -y
+colcon build --packages-select brain camera sub
+
+echo ""
+echo "run '. install/local_setup.bash'"
+echo "or  '. install/setup.bash'"
+echo ""
